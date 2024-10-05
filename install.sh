5698313c3291bdd1c017a8685eccd625719e984d
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2024-10-05 14:34:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,14 @@
 # Written By NEOAPPS
 # Licensed under the GNU GPL v3
 
+addtobashrc() {
+  echo "source ~/ebash/eba.sh" >> ~/.bashrc
+  echo Added to .bashrc.
+  echo Please restart the terminal before starting to use eBash.
+}
+
 echo Installing eBash...
-curl https://github.com
+curl -fsSL https://raw.githubusercontent.com/neoapps-dev/ebash/refs/heads/main/eba.sh -O ~/ebash/eba.sh
+echo Done with code $?.
+source ~/ebash/eba.sh # imports ebash
+confirm "Do you want to add it to .bashrc file to make run everywhere without importing?" addtobashrc
